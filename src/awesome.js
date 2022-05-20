a9707743db7e20f464736a34f33f520023f03061
Aladdin4u <taofikakinkumi@gmail.com>
2022-05-20 04:27:06 +0100

    aladdin commite to this repo

--- src/awesome.js
+++ src/awesome.js
@@ -2,5 +2,6 @@ export default {
   sauce: function(){
     alert('your crap website is now been awesome sauced');
     console.log('your crap website website is now awesome');
+    console.log('aladdin4u also added to your crap website');
   }
 }
